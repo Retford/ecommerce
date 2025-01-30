277ddf25ca85eb60de57602ae259d49fb7d512ce
Retford <retfordjb@gmail.com>
2025-01-30 16:06:30 -0500

    structure of the files added

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,9 @@
+import { Title } from '@/components/ui/title/Title';
+
+export default function Home() {
+  return (
+    <>
+      <Title title='Tienda' subtitle='Todos los productos' className='mb-2' />
+    </>
+  );
+}
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { titleFont } from '@/config/fonts';
+
+export default function LoginHome() {
+  return (
+    <div>
+      <h1>Hola Mundo</h1>
+      <h1 className={`${titleFont.className}`}>Hola mundo</h1>
+    </div>
+  );
+}
