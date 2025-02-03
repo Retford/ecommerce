6d201d7897f0994061cbf735a753a185229b6c74
Retford <retfordjb@gmail.com>
2025-02-03 18:04:13 -0500

    products per category page added

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,15 +1,38 @@
-import { notFound } from 'next/navigation';
+// import { notFound } from 'next/navigation';
+
+import { ProductGrid } from '@/components/products/product-grid/ProductGrid';
+import { Title } from '@/components/ui/title/Title';
+import { initialData, Category } from '@/seed/seed';
 
 interface Props {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: Category }>;
 }
+
+const seedProducts = initialData.products;
 
 export default async function CategoryPage({ params }: Props) {
   const { id } = await params;
 
-  if (id === 'kids') {
-    notFound();
-  }
+  const labels: Record<Category, string> = {
+    men: 'para hombres',
+    women: 'para Mujeres',
+    kid: 'para Niños',
+    unisex: 'para todos',
+  };
 
-  return <div>Category + {id}</div>;
+  // if (id === 'kids') {
+  //   notFound();
+  // }
+
+  const products = seedProducts.filter((product) => product.gender === id);
+
+  return (
+    <>
+      <Title
+        title={`Artículos ${labels[id]}`}
+        subtitle={`Artículos de ${id}`}
+      />
+      <ProductGrid products={products} />
+    </>
+  );
 }
