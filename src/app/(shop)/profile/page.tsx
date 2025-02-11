9b1cd078e22beca33f6707934ff46201f55594a6
Retford <retfordjb@gmail.com>
2025-02-11 13:26:35 -0500

    redirection added

---
+++ src/app/(shop)/profile/page.tsx
@@ -0,0 +1,18 @@
+import { auth } from '@/auth.config';
+import { Title } from '@/components/ui/title/Title';
+import { redirect } from 'next/navigation';
+
+export default async function ProfilePage() {
+  const session = await auth();
+
+  if (!session?.user) {
+    // redirect('/auth/login?returnTo=/perfil');
+    redirect('/');
+  }
+  return (
+    <div>
+      <Title title='Perfil' />
+      <pre>{JSON.stringify(session.user, null, 2)}</pre>
+    </div>
+  );
+}
