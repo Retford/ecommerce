84706a4629f872374b2e73890635cc2e960778c3
Retford <retfordjb@gmail.com>
2025-02-11 11:42:17 -0500

    email, login added in seedData

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -11,13 +11,16 @@ async function main() {
   //   prisma.category.deleteMany(),
   // ]);
 
+  await prisma.user.deleteMany();
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
   await prisma.category.deleteMany();
 
   // Categorías
 
-  const { categories, products } = initialData;
+  const { users, categories, products } = initialData;
+
+  await prisma.user.createMany({ data: users });
 
   const categoriesData = categories.map((category) => ({ name: category }));
 
